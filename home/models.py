b1fa2c0075e356c640887e6719eb39d832b0854b
salolotsulashvili <lotsulashvilisalome2005@gmail.com>
2024-06-23 22:25:03 +0400

    add final project

---
+++ home/models.py
@@ -0,0 +1,67 @@
+
+from django.contrib.auth.models import User
+from django.db import models
+import uuid
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4   , editable=False , primary_key=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+
+
+class Amenities(BaseModel):
+    amenity_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.amenity_name
+
+class Hotel(BaseModel):
+    hotel_name= models.CharField(max_length=100)
+    hotel_price = models.IntegerField()
+    description = models.TextField()
+    amenities = models.ManyToManyField(Amenities)
+    room_count = models.IntegerField(default=10)
+
+    def __str__(self) -> str:
+        return self.hotel_name
+
+
+class HotelImages(BaseModel):
+    hotel= models.ForeignKey(Hotel ,related_name="images", on_delete=models.CASCADE)
+    images = models.ImageField(upload_to="hotels")
+
+
+
+class HotelBooking(BaseModel):
+    hotel= models.ForeignKey(Hotel  , related_name="hotel_bookings" , on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="user_bookings" , on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    booking_type= models.CharField(max_length=100,choices=(('Pre Paid' , 'Pre Paid') , ('Post Paid' , 'Post Paid')))
+
+
+
+
+
+from django.db import models
+from .models import Service
+
+class Price(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.service} - {self.price} ({self.date})"
+
+
+
+
+
+
